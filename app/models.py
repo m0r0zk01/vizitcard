396e84025fd5949c6ac140be0e621c284a211b18
m0r0zk01 <47718803+m0r0zk01@users.noreply.github.com>
2020-09-23 22:38:20 +0300

    initial models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Card(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    description = models.TextField()
+    telegram = models.TextField()
+    vk = models.TextField()
+    whats_app = models.TextField()
+    telephone = models.TextField()
+    url = models.TextField()
+
+
+class Worker(User):
+    position = models.TextField()
+    photo = models.ImageField()
+    work_card = models.ForeignKey(Card, on_delete=models.SET_NULL, null=True)
+
+
+class Organization(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    description = models.TextField()
+
+    card = models.ForeignKey(Card, on_delete=models.SET_NULL, null=True)
+
+    workers = models.ManyToManyField
